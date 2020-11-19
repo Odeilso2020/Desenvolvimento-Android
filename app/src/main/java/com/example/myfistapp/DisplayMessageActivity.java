b079f59532b8bf1da4dfa47f82d2c15450da229d
Odeilso Denis Leite <odeilson_2014@hotmail.com>
2020-11-19 20:48:31 -0300

    primeiro commit

---
+++ app/src/main/java/com/example/myfistapp/DisplayMessageActivity.java
@@ -0,0 +1,21 @@
+package com.example.myfistapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class DisplayMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_message);
+
+        String message = getIntent().getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        TextView text = (TextView) findViewById(R.id.activity_display_message);
+        text.setText(message);
+    }
+}
